85bdffd47542ccb1c48c96d759a46417ca257857
Shamshod Nematullayev <maxxadsen@gmail.com>
2025-02-03 09:33:38 +0500

    vaqtinchalik https

--- src/store/constant.js
+++ src/store/constant.js
@@ -2,4 +2,4 @@
 export const gridSpacing = 3;
 export const drawerWidth = 260;
 export const appDrawerWidth = 320;
-export const SERVER_URL = 'http://45.92.173.48:5000/api'; //192.168.1.138 | 45.92.173.48
+export const SERVER_URL = ' https://0c5f-45-92-173-48.ngrok-free.app/api'; //192.168.1.138 | 45.92.173.48
