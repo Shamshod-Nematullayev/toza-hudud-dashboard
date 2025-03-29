4e6ec0ca2c22d0aeaf8f573520a183c0bbfef00a
Shamshod Nematullayev <maxxadsen@gmail.com>
2025-03-29 22:47:10 +0500

    odam soni bekor qilish funksiyasi

--- src/views/billing/XatlovDalolatnomalar/DataTable.jsx
+++ src/views/billing/XatlovDalolatnomalar/DataTable.jsx
@@ -3,8 +3,9 @@ import React, { useEffect, useState } from 'react';
 import api from 'utils/api';
 import DoDisturbAltOutlinedIcon from '@mui/icons-material/DoDisturbAltOutlined';
 import { IconButton, Tooltip } from '@mui/material';
+import { toast } from 'react-toastify';
 
-function DataTable({ rows, paging, setPaging, rowsMeta = {} }) {
+function DataTable({ rows, paging, setPaging, rowsMeta = {}, setRows }) {
   const [mahallalar, setMahallalar] = useState([]);
 
   useEffect(() => {
@@ -13,7 +14,29 @@ function DataTable({ rows, paging, setPaging, rowsMeta = {} }) {
     });
   }, []);
 
-  const handleClickCancelButton = (document) => {};
+  const handleClickCancelButton =async (document) => {
+    try {
+      const asos = prompt(`Siz haqiqatan ham ushbu (${document.documentNumber}) dalolatnomani bekor qilmoqchimisiz? Bekor qilish sababini yozing`)
+      if(asos) {
+        await api.put("/yashovchi-soni-xatlov/cancel-document/" + document._id, {
+          body: {
+            cancelDescription: asos,
+          },
+        });
+       const newRows = rows.map(row => {
+        if(row._id === document._id) {
+          row.isCancel = true
+        }
+        return row
+       }) 
+       setRows(newRows)
+       toast.info("Bekor qilindi")
+      }
+    } catch (error) {
+      console.log(error);
+      
+    }
+  };
   return (
     <DataGrid
       columns={[
@@ -39,12 +62,17 @@ function DataTable({ rows, paging, setPaging, rowsMeta = {} }) {
           renderCell: ({ row }) => row.request_ids.length
         },
         {
+          field: 'status',
+          headerName: 'Holat',
+          renderCell: ({ row }) => row.isCancel ? 'Bekor qilingan' : "Aktiv"
+        },
+        {
           field: '_',
           headerName: 'Harakatlar',
           renderCell: ({ row }) => (
             <>
               <Tooltip title={'Bekor qilish'}>
-                <IconButton color={'error'}>
+                <IconButton color={'error'} onClick={() => handleClickCancelButton(row)} disabled={row.isCancel}>
                   <DoDisturbAltOutlinedIcon />
                 </IconButton>
               </Tooltip>
