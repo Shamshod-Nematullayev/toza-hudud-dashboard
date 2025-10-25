db0edff8e7865661104ed7b6f9e43a60dd4f2ca6
Shamshod-Nematullayev <maxxadsen@gmail.com>
2025-10-25 20:45:14 +0500

    add: EditableComponent + GPSDalolatnoma/PrintSection

--- src/views/gpsMonitoring/CreateGpsDalolatnoma/index.tsx
+++ src/views/gpsMonitoring/CreateGpsDalolatnoma/index.tsx
@@ -3,13 +3,25 @@ import React from 'react';
 import MainCard from 'ui-component/cards/MainCard';
 import PrintSection from './PrintSection';
 import SideBarGpsCreateGpsDalolatnoma from './SideBarGpsCreateGpsDalolatnoma';
+import { useGpsDalolatnomaStore } from './useStore';
+import { Company } from 'views/billing/Blanks';
 
 function CreateGpsDalolatnoma() {
+  const { date, cars, responsibleCarId, currentCarId, currentCarDriver, responsibleCarDriver, description, document } =
+    useGpsDalolatnomaStore();
+  const company = JSON.parse(localStorage.getItem('company') as string) as Company;
   return (
     <MainCard>
       <Grid container spacing={2}>
         <Grid item xs={9}>
-          <PrintSection />
+          <PrintSection
+            company={company}
+            date={date}
+            responsibleCar={cars.find((c) => c.id === responsibleCarId)}
+            responsibleCarDriverName={responsibleCarDriver?.fullName}
+            currentCar={cars.find((c) => c.id === currentCarId)}
+            currentCarDriverName={currentCarDriver?.fullName}
+          />
         </Grid>
         <Grid item xs={3}>
           <SideBarGpsCreateGpsDalolatnoma />
